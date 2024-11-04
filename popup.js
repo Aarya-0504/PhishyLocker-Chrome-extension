cbf6a21ad73e738fa944922ff83353d4842811cf
Aarya_P <aarya.patil@spit.ac.in>
2024-11-05 01:02:21 +0530

    update url

--- popup.js
+++ popup.js
@@ -5,21 +5,21 @@ document.addEventListener('DOMContentLoaded', function () {
   // Send a message to the content script to get the current tab's URL
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     const url = tabs[0].url;
-    const thirdSlashIndex = url.indexOf('/', url.indexOf('/') + 2); // Find index of third slash
-    const topDomain = url.substring(0, thirdSlashIndex); // Extract all text before third slash
+    // const thirdSlashIndex = url.indexOf('/', url.indexOf('/') + 2); // Find index of third slash
+    // const topDomain = url.substring(0, thirdSlashIndex); // Extract all text before third slash
     
     console.log(topDomain);
     
-    fetch('https://extension-server-58gy.onrender.com/predict/', {
+    fetch('http://15.207.54.78/analyze-url/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ url: topDomain })
+      body: JSON.stringify({ url: url })
     })
     .then(response => response.json())
     .then(data => {
-      const prediction = data.prediction; // Assuming the server responds with { "prediction": "some_value" }
+      const prediction = data.results; // Assuming the server responds with { "prediction": "some_value" }
       console.log(prediction);
       messageElement.textContent = `Prediction: ${prediction}`;
       messageElement2.textContent = `URL: ${url}`;
